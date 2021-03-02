3cf03b8a64920ee263293778036f80390ee49e28
Dailson Gomes <daylsongomes@gmail.com>
2021-03-02 16:37:48 -0300

    H2 Database

--- src/main/java/com/dailson/cursomc/domain/Category.java
+++ src/main/java/com/dailson/cursomc/domain/Category.java
@@ -2,6 +2,11 @@ package com.dailson.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,10 +19,14 @@ import lombok.ToString;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
 @Data
+
+@Entity
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
 	private Integer id;
 	private String name;
