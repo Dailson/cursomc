8c8c974d59a2f52de92fd3e830cc1032cc2cbd4c
Dailson Gomes <daylsongomes@gmail.com>
2021-03-02 15:11:14 -0300

    The project was created

---
+++ src/test/java/com/dailson/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailson.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
