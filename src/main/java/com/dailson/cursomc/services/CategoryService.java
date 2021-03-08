c2250b48da96609bdaf3d45eba686569bc1987c3
Dailson Gomes <daylsongomes@gmail.com>
2021-03-08 16:41:43 -0300

    Deleting a Category with DELETE

--- src/main/java/com/dailson/cursomc/services/CategoryService.java
+++ src/main/java/com/dailson/cursomc/services/CategoryService.java
@@ -3,10 +3,12 @@ package com.dailson.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.dailson.cursomc.domain.Category;
 import com.dailson.cursomc.repositories.CategoryRepository;
+import com.dailson.cursomc.services.exceptions.DataIntegrityException;
 import com.dailson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,6 +32,17 @@ public class CategoryService {
 	public Category update(Category obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		
+		try {			
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("It's no possible to delete a Category which has Product");
+		}
+
 	}
 
 }
